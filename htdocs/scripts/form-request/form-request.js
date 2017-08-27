225671c4e1e3e510016df546f90b21f36db03802
Alexander Svistunov <alexsvistunov73@gmail.com>
2017-08-27 12:39:16 +0300

    (js) start validation form-request

---
+++ htdocs/scripts/form-request/form-request.js
@@ -0,0 +1,48 @@
+"use strict";
+
+//валидация формы
+
+var form = document.querySelector(".js-form-request");
+var email = document.getElementById("form-request-email");
+var userName = document.getElementById("form-request-name");
+var selectCountry = document.getElementById("form-request-select-country");
+var checkbox = document.getElementById("form-request-checkbox-agree");
+var error = document.getElementsByClassName("error");
+
+var adrPattern = /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}/i;
+var namePattern = /[a-zа-я- ]/i;
+
+form.addEventListener("submit", valid);
+
+function valid(e) {
+  e.preventDefault();
+  if (adrPattern.test(email.value) == false) {
+    email.classList.add("error");
+  } else {
+    email.classList.remove("error");
+  }
+
+  if (namePattern.test(userName.value) == false) {
+    userName.classList.add("error");
+  } else {
+    userName.classList.remove("error");
+  }
+
+  if (selectCountry.value == "default") {
+    selectCountry.classList.add("error");
+  } else {
+    selectCountry.classList.remove("error");
+  }
+
+  if (!checkbox.checked) {
+    checkbox.classList.add("error");
+  } else {
+    checkbox.classList.remove("error");
+  }
+
+  if (error.length > 0) {
+    alert(error.length + " fail");
+  } else {
+    window.location = "https://www.google.com";
+  }
+}
